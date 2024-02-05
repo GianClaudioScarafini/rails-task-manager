784458cebb593e9ef0b65dcdb6231b2cccb937a9
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-02-05 07:54:58 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Create Read Update Delate
+
+  # Read all
+  get "tasks",            to: "tasks#index"
+
+  #Create
+  get "tasks/:id",       to: "tasks#show",        as: :task
+  get "tasks/new",       to: "tasks#new",         as: :new_task
+  get "tasks",           to: "tasks#create"
+
+  #Update
+  get "tasks/:id/edit",  to: "tasks#edit",        as: :edit_task
+  patch "tasks/:id",     to: "tasks#update"
+
+
+  #Delate
+  delete "tasks/:id",    to: "tasks#destroy"
+
+
 end
